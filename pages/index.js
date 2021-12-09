fe65688e8a58cf711fc375f138171c6a2a982bfc
vivawal <abdullah.awal@vivacomsolutions.com>
2021-12-10 01:34:27 +0600

    Next/link covered

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,24 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
 const Home = () => {
-    return (<h1>Home</h1>);
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/product");
+  };
+
+  return (
+    <>
+      <h1>Home</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+      <Link href="/product">
+        <a>Product</a>
+      </Link>
+      <button onClick={handleClick}>Place order</button>
+    </>
+  );
 };
 
 export default Home;
